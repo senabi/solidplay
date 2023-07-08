67d67874bfed4ba0043ecb3a50cf17c888ecfba7
Ivan B <sensenabi@gmail.com>
2023-07-08 17:07:36 -0500

    chore: clean up

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,40 +1,13 @@
 import { type VoidComponent, Suspense, Show } from "solid-js";
-import { A } from "solid-start";
 import { trpc } from "~/utils/trpc";
 import { createSession, signOut, signIn } from "@solid-auth/base/client";
 
 const Home: VoidComponent = () => {
   const hello = trpc.example.hello.useQuery(() => ({ name: "from tRPC" }));
   return (
-    <main class="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#026d56] to-[#152a2c]">
-      <div class="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 class="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span class="text-[hsl(88,_77%,_78%)]">JD</span> App
-        </h1>
-        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <A
-            class="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://start.solidjs.com"
-            target="_blank"
-          >
-            <h3 class="text-2xl font-bold">Solid Start →</h3>
-            <div class="text-lg">
-              Learn more about Solid Start and the basics.
-            </div>
-          </A>
-          <A
-            class="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://github.com/orjdev/create-jd-app"
-            target="_blank"
-          >
-            <h3 class="text-2xl font-bold">JD End →</h3>
-            <div class="text-lg">
-              Learn more about Create JD App, the libraries it uses, and how to
-              deploy it
-            </div>
-          </A>
-        </div>
-        <div class="flex flex-col items-center gap-2">
+    <>
+      <main class="flex min-h-[100svh] flex-col items-center justify-center bg-gradient-to-b from-[#026d56] to-[#152a2c]">
+        <div class="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <p class="text-2xl text-white">
             {hello.data ?? "Loading tRPC query"}
           </p>
@@ -42,8 +15,9 @@ const Home: VoidComponent = () => {
             <AuthShowcase />
           </Suspense>
         </div>
-      </div>
-    </main>
+      </main>
+      <footer>hi</footer>
+    </>
   );
 };
 
